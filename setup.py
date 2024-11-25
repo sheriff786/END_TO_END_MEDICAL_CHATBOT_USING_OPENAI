3f20f627182434fce052ce771a7b1cd8c4709dc1
Sheriff786 <mohdsheriff27021996@gmail.com>
2024-11-25 13:46:34 +0530

    folder structure

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'Generative AI Project',
+    version= '0.0.0',
+    author= 'Mohammad Sheriff',
+    author_email= 'mohdsheriff27021996gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
